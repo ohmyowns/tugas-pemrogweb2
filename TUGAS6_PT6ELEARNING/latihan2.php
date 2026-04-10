2fa46b4f9bcd0d6c22b0023eb0ecac4c9e33776d
ohmyowns <110959045+ohmyowns@users.noreply.github.com>
2026-04-10 16:25:39 +0700

    Add files via upload
    Kumpulan Tugas 6 di Elearning Pertemuan 6

---
+++ TUGAS6_PT6ELEARNING/latihan2.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Latihan 2 Array</title>
+</head>
+<body>
+
+<?php
+$x = array("one", "two", "three");
+
+foreach ($x as $value) {
+    echo $value . "<br />";
+}
+?>
+
+</body>
+</html>
